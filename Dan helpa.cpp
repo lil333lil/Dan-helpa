e09a5f95dca76cbb5cc8d04467dbd3b07ec0f119
Артем Попов <lil333lil@yandex.ru>
2022-12-10 06:28:39 +0300

    Добавьте файлы проекта.

---
+++ Dan helpa.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <chrono>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+int main() {
+   /* unsigned long long int k = 0;
+    for (unsigned long long int i = 0; i < 100000000000; i++) {
+        if (i % 100000 == 0)
+            printf("%k\n", k);
+            
+        k += 1;
+    }
+    cout << k;*/
+
+    vector <int> 
+}
