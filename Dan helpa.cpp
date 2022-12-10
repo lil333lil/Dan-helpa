04e1c02d07163fc9ae8b05a5c5e962d6982958b1
Артем Попов <66265764+lil333lil@users.noreply.github.com>
2022-12-10 06:38:33 +0300

    Revert "Test"

--- Dan helpa.cpp
+++ Dan helpa.cpp
@@ -6,5 +6,14 @@
 
 using namespace std;
 int main() {
-   cout << "Artem privet" << endl;
+   /* unsigned long long int k = 0;
+    for (unsigned long long int i = 0; i < 100000000000; i++) {
+        if (i % 100000 == 0)
+            printf("%k\n", k);
+            
+        k += 1;
+    }
+    cout << k;*/
+
+    vector <int> 
 }
